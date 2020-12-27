b2c52517ae8979d0727ad801f86a4d83871d00ca
Antonio <Tenundor@gmail.com>
2020-12-27 18:38:42 +0300

    Добавил функцию возращающую генератор вакансий superjob

--- main.py
+++ main.py
@@ -1,30 +1,25 @@
 from dotenv import load_dotenv
 from itertools import count
+from math import ceil
 import os
 import requests
 from pprint import pprint
 
 
-def get_filtered_vacancies_hh(
-        user_agent_name,
-        specialization_id="",
-        area="",
-        period="",
-        search_text="",
-):
+def get_filtered_vacancies_hh(user_agent_name, filtering_options={}):
     hh_api_url = "https://api.hh.ru/vacancies"
-    vacancies_request_parameters = {
-        "specialization": specialization_id,
-        "area": area,
-        "period": period,
-        "text": search_text,
-    }
+    vacancies_request_parameters = dict({
+        "specialization": "",
+        "area": "",
+        "period": "",
+        "text": "",
+    }, **filtering_options)
     vacancies_request_header = {"User-Agent": user_agent_name}
     total_pages_number = 0
-    for vacancies_page_number in count():
-        if total_pages_number and vacancies_page_number >= total_pages_number:
+    for vacancies_page_index in count():
+        if total_pages_number and vacancies_page_index >= total_pages_number:
             break
-        vacancies_request_parameters["page"] = vacancies_page_number
+        vacancies_request_parameters["page"] = vacancies_page_index
         vacancies_page_response = requests.get(
             hh_api_url,
             params=vacancies_request_parameters,
@@ -33,7 +28,38 @@ def get_filtered_vacancies_hh(
         vacancies_page_response.raise_for_status()
         vacancies_page_json = vacancies_page_response.json()
         total_pages_number = vacancies_page_json["pages"]
-        yield vacancies_page_json
+        vacancies_page_number = vacancies_page_index + 1
+        print("{} from {} downloaded.".format(vacancies_page_number, total_pages_number))
+        yield vacancies_page_json["items"]
+
+
+def get_filtered_vacancies_superjob(authorization_key, filtering_options={}):
+    superjob_api_url = "https://api.superjob.ru/2.0/vacancies"
+    superjob_authorisation_header = {"X-Api-App-ID": authorization_key}
+    superjob_request_parameters = dict({
+        "town": "",
+        "catalogues": "",
+        "count": 20,
+    }, **filtering_options)
+    is_more_pages = True
+    for page_index in count():
+        if not is_more_pages:
+            break
+        superjob_request_parameters["page"] = page_index
+        superjob_response = requests.get(
+            superjob_api_url,
+            params=superjob_request_parameters,
+            headers=superjob_authorisation_header,
+        )
+        superjob_response.raise_for_status()
+        superjob_response_json = superjob_response.json()
+        page_number = page_index + 1
+        vacancies_per_page = superjob_request_parameters["count"]
+        total_vacancies_quantity = superjob_response_json["total"]
+        total_pages_quantity = ceil(total_vacancies_quantity / vacancies_per_page)
+        print("{} from {} pages downloaded".format(page_number, total_pages_quantity))
+        is_more_pages = superjob_response_json["more"]
+        yield superjob_response_json["objects"]
 
 
 def predict_salary(salary_from, salary_to):
@@ -72,39 +98,22 @@ def predict_average_salary(vacancies_predicted_salary):
     }
 
 
-def predict_average_rub_salary_hh(hh_vacancies_generator):
+def predict_average_rub_salary_hh(hh_vacancies_generator):  # TODO: избавиться от функций hh и sj
     vacancies_predicted_rub_salary = []
     for hh_vacancies_page in hh_vacancies_generator:
-        current_page_number = hh_vacancies_page["page"] + 1
-        total_pages_number = hh_vacancies_page["pages"]
-        print("{} from {} pages downloaded.".format(current_page_number, total_pages_number))
         vacancies_predicted_rub_salary += [
-            predict_rub_salary_hh(vacancy) for vacancy in hh_vacancies_page["items"]
+            predict_rub_salary_hh(vacancy) for vacancy in hh_vacancies_page
         ]
     predict_average_salary(vacancies_predicted_rub_salary)
 
 
-def get_filtered_vacancies_superjob(
-        authorization_key,
-        town_superjob_id,
-        catalogues_superjob,
-
-):
-    superjob_api_url = "https://api.superjob.ru/2.0/vacancies"
-    superjob_authorisation_header = {
-        "X-Api-App-ID": authorization_key
-    }
-    superjob_response_parameters = {
-        "town": town_superjob_id,
-        "catalogues": catalogues_superjob,
-    }
-    superjob_request = requests.get(
-        superjob_api_url,
-        params=superjob_response_parameters,
-        headers=superjob_authorisation_header,
-    )
-    superjob_request.raise_for_status()
-    return superjob_request.json()
+def predict_average_rub_salary_sj(sj_vacancies_generator):
+    vacancies_predicted_rub_salary = []
+    for sj_vacancies_page in sj_vacancies_generator:
+        vacancies_predicted_rub_salary += [
+            predict_rub_salary_sj(vacancy) for vacancy in sj_vacancies_page["objects"]
+        ]
+    predict_average_salary(vacancies_predicted_rub_salary)
 
 
 if __name__ == "__main__":
@@ -135,13 +144,15 @@ if __name__ == "__main__":
     moscow_superjob_id = 4
     superjob_programming_category_key = 48
     superjob_vacancies = get_filtered_vacancies_superjob(
-        superjob_api_key,
-        town_superjob_id=moscow_superjob_id,
-        catalogues_superjob=superjob_programming_category_key
-    )["objects"]
+        superjob_api_key, filtering_options={
+            "town": moscow_superjob_id,
+            "catalogues": superjob_programming_category_key,
+        }
+    )
 
-    for superjob_vacancy in superjob_vacancies:
-        vacancy_name = superjob_vacancy["profession"]
-        vacancy_town = superjob_vacancy["town"]["title"]
-        predicted_salary_sj = predict_rub_salary_sj(superjob_vacancy)
-        print(vacancy_name, vacancy_town, predicted_salary_sj, sep=", ")
+    for superjob_vacancy_page in superjob_vacancies:
+        for superjob_vacancy in superjob_vacancy_page:
+            vacancy_name = superjob_vacancy["profession"]
+            vacancy_town = superjob_vacancy["town"]["title"]
+            predicted_salary_sj = predict_rub_salary_sj(superjob_vacancy)
+            print(vacancy_name, vacancy_town, predicted_salary_sj, sep=", ")
