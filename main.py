f37aedb0b2e2a1c551d07235cb831a40a5ad8a34
Antonio <Tenundor@gmail.com>
2021-01-06 13:39:10 +0300

    Вернул функции подготовки и печати таблиц в main.py

--- main.py
+++ main.py
@@ -5,12 +5,45 @@ from headhunter_utils import get_monthly_moscow_vacancies_generator_hh
 from headhunter_utils import predict_average_rub_salary_hh
 from superjob_utils import get_monthly_moscow_vacancies_generator_sj
 from superjob_utils import predict_average_rub_salary_sj
-from utils import print_vacancies_average_salary_table
+from terminaltables import AsciiTable
+
+
+def prepare_average_salary_for_table_print(vacancies_average_salary):
+    column_headers = [
+        "Язык программирования",
+        "Вакансий найдено",
+        "Вакансий обработано",
+        "Средняя зарплата",
+    ]
+    vacancies_average_salary_list = [column_headers]
+    for programming_language, average_salary in vacancies_average_salary.items():
+        table_row = [
+            programming_language,
+            average_salary["vacancies_found"],
+            average_salary["vacancies_processed"],
+            average_salary["average_salary"],
+        ]
+        vacancies_average_salary_list.append(table_row)
+    return vacancies_average_salary_list
+
+
+def print_vacancies_average_salary_table(
+        vacancies_average_salary,
+        table_title="",
+):
+    average_salary_list = prepare_average_salary_for_table_print(
+        vacancies_average_salary
+    )
+    average_salary_table_instance = AsciiTable(
+        average_salary_list,
+        title=table_title,
+    )
+    print(average_salary_table_instance.table)
 
 
 programming_languages = [
-        "TypeScript", "Swift", "Scala", "Objective-C", "Shell", "JavaScript",
-        "Go", "C", "C#", "C++", "PHP", "Ruby", "Python", "Java",
+        "TypeScript", "Swift"#, "Scala", "Objective-C", "Shell", "JavaScript",
+        #"Go", "C", "C#", "C++", "PHP", "Ruby", "Python", "Java",
     ]
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -1,6 +1,3 @@
-from terminaltables import AsciiTable
-
-
 def predict_salary(salary_from, salary_to):
     if not salary_from and not salary_to:
         return None
@@ -29,37 +26,3 @@ def predict_average_salary(vacancies_predicted_salary):
         "vacancies_processed": processed_vacancies_quantity,
         "average_salary": average_salary,
     }
-
-
-def prepare_average_salary_for_table_print(vacancies_average_salary):
-    column_headers = [
-        "Язык программирования",
-        "Вакансий найдено",
-        "Вакансий обработано",
-        "Средняя зарплата",
-    ]
-    vacancies_average_salary_list = [column_headers]
-    for programming_language, average_salary in vacancies_average_salary.items():
-        table_row = [
-            programming_language,
-            average_salary["vacancies_found"],
-            average_salary["vacancies_processed"],
-            average_salary["average_salary"],
-        ]
-        vacancies_average_salary_list.append(table_row)
-    return vacancies_average_salary_list
-
-
-def print_vacancies_average_salary_table(
-        vacancies_average_salary,
-        table_title="",
-):
-    average_salary_list = prepare_average_salary_for_table_print(
-        vacancies_average_salary
-    )
-    average_salary_table_instance = AsciiTable(
-        average_salary_list,
-        title=table_title,
-    )
-    print(average_salary_table_instance.table)
-
