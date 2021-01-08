722293a2aa9291bb80b4212ec0b8d9e0037334e3
Antonio <Tenundor@gmail.com>
2021-01-08 21:47:05 +0300

    Высушил код функции predict_salary

--- utils.py
+++ utils.py
@@ -1,11 +1,9 @@
 def predict_salary(salary_from, salary_to):
-    if not salary_from and not salary_to:
-        return None
-    if not salary_from:
+    if not salary_from and salary_to:
         return int(salary_to * 0.8)
-    if not salary_to:
+    if not salary_to and salary_from:
         return int(salary_from * 1.2)
-    else:
+    if salary_from and salary_to:
         return int((salary_from + salary_to) / 2)
 
 
